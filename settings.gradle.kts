df23b56dc43738c0827efd2dbf9afaa0f24971cb
aimdie <473240418@qq.com>
2021-07-09 00:53:59 +0800

    将模块名修改为项目类型名称

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,6 @@
 
 
 //rootProject.name = ""
-include(":z_common")
-include(":z_module")
-
-//gradle.beforeProject {
-//  loadLocalProperties()
-//  extra.set("signing.keyId",System.getenv("SIGN_ID"))
-//  extra.set("signing.password",System.getenv("SIGN_PASSWORD"))
-//  extra.set("signing.secretKeyRingFile",System.getenv("SIGN_FILE"))
-//}
-//fun Project.loadLocalProperties() {
-//  val localPropertiesFile: File = rootDir.resolve("local.properties")
-//  if (localPropertiesFile.exists()) {
-//    val localProperties = java.util.Properties()
-//    localProperties.load(localPropertiesFile.inputStream())
-//    localProperties.forEach { (k, v) -> if (k is String) project.extra.set(k, v) }
-//  }
-//}
+include(":z-kotlin-common")
+include(":z-kotlin-multilatform")
 
