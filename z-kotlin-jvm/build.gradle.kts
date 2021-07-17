a1e998acb0122ee9d25302fcba2981d5abc605ad
aimdie <473240418@qq.com>
2021-07-17 23:22:48 +0800

    cn.ace

--- z-kotlin-jvm/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-  kotlin("jvm")
-}
-
-dependencies {
-//  implementation("org.junit.jupiter:junit-jupiter:5.7.0")
-//  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.5.0")
-//  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
-}
