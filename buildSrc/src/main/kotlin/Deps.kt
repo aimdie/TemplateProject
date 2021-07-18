c871cb1b21cfd1188ccaf8d31c6ff80c54e64ef7
aimdie <473240418@qq.com>
2021-07-18 19:00:01 +0800

    cn.ace

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,15 +1,31 @@
 object Deps {
   object Kotlin {
-    val testCommon = "org.jetbrains.kotlin:kotlin-test-common:${Versions.kotlinVersion}"
-    val testAnnotationsCommon =
-      "org.jetbrains.kotlin:kotlin-test-annotations-common:${Versions.kotlinVersion}"
-    val testJvm = "org.jetbrains.kotlin:kotlin-test:${Versions.kotlinVersion}"
-    val testJvmJunit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
+    val stdlib =
+      "org.jetbrains.kotlin:kotlin-stdlib:+"
   }
+  
+  object Kotlinx {
+    /**
+     * 自动导入"serializationCore"。
+     */
+    val serializationJson =
+      "org.jetbrains.kotlinx:kotlinx-serialization-json:+"
+    val coroutinesCore =
+      "org.jetbrains.kotlinx:kotlinx-coroutines-core:+"
+  }
+  
+  object KotlinTest {
+    
+    /**
+     * 导入后可调试。
+     */
+    val junit =
+      "org.jetbrains.kotlin:kotlin-test-junit:+"
+  }
+  
   object Test {
-    val junit = "junit:junit:${Versions.junit}"
-    val kotlinTest = "org.jetbrains.kotlin:kotlin-test:${Versions.kotlinVersion}"
-    val kotlinJunit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
+    val junit =
+      "junit:junit:+"
   }
   
   //////////////////////////////////////////////////
@@ -18,18 +34,5 @@ object Deps {
     val utilIo = "org.jetbrains.kotlin:kotlin-util-io:+"
     val serializationRuntime = "org.jetbrains.kotlinx:kotlinx-serialization-runtime:+"
     val serializationProtobuf = "org.jetbrains.kotlinx:kotlinx-serialization-protobuf:+"
-    
-    /**
-     * 导入任何“serialization”后，自动导入该模块。
-     */
-    val serializationCore = "org.jetbrains.kotlinx:kotlinx-serialization-core:+"
-    
-    /**
-     * 能引用，但是导入后看不见库。
-     */
-    val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines:+"
-    val stdlib =
-      "org.jetbrains.kotlin:kotlin-stdlib-common:+"
-    val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core-common:+"
   }
 }
